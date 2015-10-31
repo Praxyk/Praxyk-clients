0844d95fc12df639730da683200f07ad34072a8e
Nicholas Church <Nicholas@nchurch.local>
2015-10-30 17:20:57 -0700

    initial commit detailing the start of the praxyk-client code

---
+++ python/praxyk/praxyk.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+## @auth John Allard, Nick Church
+## @date Oct 2015
+## @github https://github.com/jhallard/praxyk
+## @license MIT
+
+#import praxyk_results
+#import praxyk_transaction
+#import praxyk_user
+import requests
+import sys, os
+import argparse
+import json
+
+PRAXYK_WEBSITE = 'http://api.praxyk.com'
+
+class PraxykException(Exception):
+	def __init__(self, message='', praxyk_instance=None, errors=None):
+		super(Exception, self).__init__()
+		self.errors = errors
+
+
+class Praxyk:
+	def __init__(self, username=None, password=None, authok=None):
+		if username and password:
+			self.username = username
+			self.password = password
+			#attempt login, if it fails auth, raise an exception
+			response = requests.get('%s/users/' % PRAXYK_WEBSITE, auth=(username, password))
+			print 'response ', response
+			raise PraxykException(message='Could not confirm user authorization with given username/password combo or authorization token', praxyk_instance=self)
+		elif authtok:
+			self.authtok = authtok
+			# attempt login
+			# if not authorized:
+			raise PraxykException(message='Could not confirm user authorization with given username and password or authorization token', praxyk_instance=self)
+		else:
+			raise PraxykException(message='No username and password or authorization token present.\nIf you do not have a valid username and password or authorization token, refer to: %s\n' % PRAXYK_WEBSITE, praxyk_instance=self)
+	#def login():
