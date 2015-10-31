332a8ebd9d80132ae3ef2786568e6830dd82c942
Nicholas Church <Nicholas@nchurch.local>
2015-10-31 09:02:30 -0700

    another commit with some changes to the architecture, to be merged with John's code later.

--- python/praxyk/praxyk.py
+++ python/praxyk/praxyk.py
@@ -17,9 +17,8 @@ PRAXYK_WEBSITE = 'http://api.praxyk.com'
 
 class PraxykException(Exception):
 	def __init__(self, message='', praxyk_instance=None, errors=None):
-		super(Exception, self).__init__()
+		super(Exception, self).__init__(message)
 		self.errors = errors
-
 
 def login(username=None, password=None):
 	if username and password:
@@ -27,29 +26,33 @@ def login(username=None, password=None):
 			return requests.get('%s/users/' % PRAXYK_WEBSITE, auth=(username, password))
 		except Exception as e:
 			print 'Error verifying login credentials'
-			return
+			return None
 	else:
 		print 'Username or password missing, only one supplied'
-
+		return None
 
 class Praxyk:
-	class PraxykUser:
-		def __init__(self, username=None, password=None, authok=None):
-			if username and password:
-				self.username = username
-				self.password = password
-				#attempt login, if it fails auth, raise an exception
-				print 'response ', response
-				raise PraxykException(message='Could not confirm user authorization with given username/password combo or authorization token', praxyk_instance=self)
-			elif authtok:
-				self.authtok = authtok
-				# attempt login
-				# if not authorized:
-				raise PraxykException(message='Could not confirm user authorization with given username and password or authorization token', praxyk_instance=self)
+	def __init__(self, username=None, password=None, authok=None):
+		if username and password:
+			#attempt login, if it fails auth, raise an exception
+			try:
+				response = login(username, password)
+			except PraxykException as e:
+				return e
+			if response.status_code is not 200:
+				raise PraxykException(message='Could not confirm user authorization with given username password combo or authorization token. The Praxyk API server returned status code %d' % response.status_code, praxyk_instance=self)
 			else:
-				raise PraxykException(message='No username and password or authorization token present.\nIf you do not have a valid username and password or authorization token, refer to: %s\n' % PRAXYK_WEBSITE, praxyk_instance=self)
-		def get():
+				print 'success!'
+				return
+				#self.users = PraxykUser()
 
-		def update()
-	def __init__():
+		elif authtok:
+			self.authtok = authtok
+			# attempt login
+			# if not authorized:
+			raise PraxykException(message='Could not confirm user authorization with given username and password or authorization token', praxyk_instance=self)
+		else:
+			raise PraxykException(message='No username and password or authorization token present.\nIf you do not have a valid username and password or authorization token, refer to: %s\n' % PRAXYK_WEBSITE, praxyk_instance=self)
 
+p = Praxyk(username='nickchurch10@gmail.com', password='praxykisdope')
+print 'Praxyk: ', p
