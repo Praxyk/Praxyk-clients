57c6c6cbd75970cfe332dc4d237e6cac44da2a01
Ryan Coley <rcoley93@gmail.com>
2015-11-15 00:50:47 -0800

    Finished Constructor and Requires Auth, added unit testing framework

--- libs/javascript/praxyk.js
+++ libs/javascript/praxyk.js
@@ -13,23 +13,69 @@ class Praxyk{
 			COUPON_URL: BASE_URL + "coupon/"; 
 			OCR_URL: POD_URL + "ocr/";
 			SPAM_URL: POD_URL + "bayes_spam/";
-			FACE_DETECTION_URL: POD_URL + "/";
+			FACE_DETECTION_URL: POD_URL + "face_detect/";
 			TRANSACTION_URL: BASE_URL + "transactions/";
 			RESULTS_URL: BASE_URL + "results/";
-		}
+		};
 		return { getURL : function(s) {
 			return URL[s];
 			}
 		}
 	}();
-
-	var username = null;
-	var password = null;
+	
 	var userid = null;
 	var token = null;
+	var email = null;
+	var password = null;
 
 	constructor(email,password){
-		api_call(URL.getURL("TOKEN_URL"),"POST".
+		//get data ready
+		var login_data = new Object();
+		login_data.email = username;
+		login_data.password = password;
+
+		var json_data = JSON.stringify(login_data);
+
+		api_call(URL.getURL("TOKEN_URL"),"POST",login_data,"text/json",function(result){
+			if(typeof result != "number"){
+				var login_json = $.parseJSON(result);
+				if(login_json.code == 200) {
+					this.userid = login_json.user.user_id;
+					this.token = login_json.token;
+					this.email = email;
+					this.password = password;
+				}
+			});
+		}else{
+			throw {
+				name: "Auth Error",
+				message: "Invalid Login Credentials!"
+			};
+		}
+	}
+	
+	var requires_auth = function(){
+		if(this.token == null && this.userid == null){
+			throw {
+				name: "Auth Error",
+				message: "No Valid Token!"
+			};
+		}
+		api_call(URL.getURL("TOKEN_URL")+"?token="+this.token,"GET",null,function(result){
+			if(typeof result == "number"){
+				if(this.email != null && this.password != null){
+					api_call(URL.getURL("TOKEN_URL"),"POST",login_data,"text/json",function(result){
+						if(typeof result != number){
+							var login_json = $.parseJSON(result);
+							if(login_json.code == 200) {
+								this.userid = login_json.user.user_id;
+								this.token = login_json.token;
+							}
+						}
+					});
+				}
+			}
+		});
 	}
 
 	function api_call(url,method,payload,content_type,callback){
@@ -38,16 +84,17 @@ class Praxyk{
 		xhr.onreadystatechange = function (){
 			if(xhr.readyState==4 && xhr.status==200){
 				callback(xhr.responseText);
-			} 
+			}else{
+					callback(xhr.status)
+			}
 		}
 			
 		xhr.open(method,url,true);
 		if(payload!=null){
 			if(content_type!=null) xhr.setRequestHeader('Content-Type',content_type);
 				xhr.send(payload);
-			}else{
-				xhr.send();
-			}
+		}else{
+			xhr.send();
 		}
-
+   }
 }
