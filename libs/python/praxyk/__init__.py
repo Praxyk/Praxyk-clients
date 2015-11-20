859d34c54749072931af1558048b7d345af6a9c4
jhallard <john@jhallard.com>
2015-11-19 17:37:33 -0800

    Fixed up client script to find correct praxyk library.
    Now can do import libs.python.praxyk to import from the interactive
    scripts' directory.

--- libs/python/praxyk/__init__.py
+++ libs/python/praxyk/__init__.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import base, praxyk, praxyk_exception, user, transaction
 
 from praxyk import Praxyk
